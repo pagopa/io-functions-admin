d1c229de7db4d442976b667a901438a5a76b8534
Francesco Persico <francescopersico@gmail.com>
2019-10-01 12:05:14 +0200

    [#168830930] CreateDevelopmentProfile function (#6)
    * Add CreateProfile function
    
    * Add openapi definition
    
    * Create an ad-hoc function to create development profiles
    
    * Change function path

---
+++ CreateDevelopmentProfile/handler.ts
@@ -0,0 +1,146 @@
+import * as express from "express";
+
+import { Context } from "@azure/functions";
+
+import { isLeft } from "fp-ts/lib/Either";
+
+import { readableReport } from "italia-ts-commons/lib/reporters";
+import {
+  IResponseErrorValidation,
+  IResponseSuccessJson,
+  ResponseErrorFromValidationErrors,
+  ResponseSuccessJson
+} from "italia-ts-commons/lib/responses";
+import { FiscalCode, SandboxFiscalCode } from "italia-ts-commons/lib/strings";
+
+import { ExtendedProfile } from "io-functions-commons/dist/generated/definitions/ExtendedProfile";
+import {
+  Profile,
+  ProfileModel,
+  RetrievedProfile
+} from "io-functions-commons/dist/src/models/profile";
+import {
+  AzureApiAuthMiddleware,
+  IAzureApiAuthorization,
+  UserGroup
+} from "io-functions-commons/dist/src/utils/middlewares/azure_api_auth";
+import { ContextMiddleware } from "io-functions-commons/dist/src/utils/middlewares/context_middleware";
+import { SandboxFiscalCodeMiddleware } from "io-functions-commons/dist/src/utils/middlewares/sandboxfiscalcode";
+import {
+  IRequestMiddleware,
+  withRequestMiddlewares,
+  wrapRequestHandler
+} from "io-functions-commons/dist/src/utils/request_middleware";
+import {
+  IResponseErrorQuery,
+  ResponseErrorQuery
+} from "io-functions-commons/dist/src/utils/response";
+
+import { DevelopmentProfile } from "../generated/definitions/DevelopmentProfile";
+
+export function toExtendedProfile(profile: RetrievedProfile): ExtendedProfile {
+  return {
+    accepted_tos_version: profile.acceptedTosVersion,
+    blocked_inbox_or_channels: profile.blockedInboxOrChannels,
+    email: profile.email,
+    is_inbox_enabled: profile.isInboxEnabled === true,
+    is_webhook_enabled: profile.isWebhookEnabled === true,
+    preferred_languages: profile.preferredLanguages,
+    version: profile.version
+  };
+}
+
+/**
+ * A middleware that extracts a DevelopmentProfile payload from a request.
+ */
+export const DeveloperProfilePayloadMiddleware: IRequestMiddleware<
+  "IResponseErrorValidation",
+  DevelopmentProfile
+> = request =>
+  new Promise(resolve => {
+    const validation = DevelopmentProfile.decode(request.body);
+    const result = validation.mapLeft(
+      ResponseErrorFromValidationErrors(ExtendedProfile)
+    );
+    resolve(result);
+  });
+
+/**
+ * Type of an CreateDevelopmentProfile handler.
+ */
+type ICreateDevelopmentProfileHandler = (
+  context: Context,
+  auth: IAzureApiAuthorization,
+  sandboxFiscalCode: SandboxFiscalCode,
+  developmentProfile: DevelopmentProfile
+) => Promise<
+  | IResponseSuccessJson<ExtendedProfile>
+  | IResponseErrorValidation
+  | IResponseErrorQuery
+>;
+
+export function CreateDevelopmentProfileHandler(
+  profileModel: ProfileModel
+): ICreateDevelopmentProfileHandler {
+  return async (context, _, sandboxFiscalCode, developmentProfilePayload) => {
+    const logPrefix = `CreateDevelopmentProfileHandler|ENTITY_ID=${sandboxFiscalCode})`;
+
+    const errorOrFiscalCode = FiscalCode.decode(sandboxFiscalCode);
+
+    if (isLeft(errorOrFiscalCode)) {
+      context.log.error(
+        `${logPrefix}|ERROR=${readableReport(errorOrFiscalCode.value)}`
+      );
+      return ResponseErrorFromValidationErrors(FiscalCode)(
+        errorOrFiscalCode.value
+      );
+    }
+
+    const fiscalCode = errorOrFiscalCode.value;
+
+    const profile: Profile = {
+      email: developmentProfilePayload.email,
+      fiscalCode,
+      isInboxEnabled: true,
+      isWebhookEnabled: true
+    };
+
+    const errorOrCreatedProfile = await profileModel.create(
+      profile,
+      profile.fiscalCode
+    );
+
+    if (isLeft(errorOrCreatedProfile)) {
+      context.log.error(
+        `${logPrefix}|ERROR=${errorOrCreatedProfile.value.body}`
+      );
+      return ResponseErrorQuery(
+        "Error while creating a new development profile",
+        errorOrCreatedProfile.value
+      );
+    }
+
+    const createdProfile = errorOrCreatedProfile.value;
+
+    context.log.verbose(`${logPrefix}|SUCCESS`);
+
+    return ResponseSuccessJson(toExtendedProfile(createdProfile));
+  };
+}
+
+/**
+ * Wraps an CreateDevelopmentProfile handler inside an Express request handler.
+ */
+export function CreateDevelopmentProfile(
+  profileModel: ProfileModel
+): express.RequestHandler {
+  const handler = CreateDevelopmentProfileHandler(profileModel);
+
+  const middlewaresWrap = withRequestMiddlewares(
+    ContextMiddleware(),
+    AzureApiAuthMiddleware(new Set([UserGroup.ApiDevelopmentProfileWrite])),
+    SandboxFiscalCodeMiddleware,
+    DeveloperProfilePayloadMiddleware
+  );
+  return wrapRequestHandler(middlewaresWrap(handler));
+}
---
+++ CreateDevelopmentProfile/index.ts
@@ -0,0 +1,64 @@
+import { Context } from "@azure/functions";
+
+import * as express from "express";
+import * as winston from "winston";
+
+import { DocumentClient as DocumentDBClient } from "documentdb";
+
+import {
+  PROFILE_COLLECTION_NAME,
+  ProfileModel
+} from "io-functions-commons/dist/src/models/profile";
+
+import * as documentDbUtils from "io-functions-commons/dist/src/utils/documentdb";
+import { getRequiredStringEnv } from "io-functions-commons/dist/src/utils/env";
+import { secureExpressApp } from "io-functions-commons/dist/src/utils/express";
+import { AzureContextTransport } from "io-functions-commons/dist/src/utils/logging";
+import { setAppContext } from "io-functions-commons/dist/src/utils/middlewares/context_middleware";
+
+import createAzureFunctionHandler from "io-functions-express/dist/src/createAzureFunctionsHandler";
+
+import { CreateDevelopmentProfile } from "./handler";
+
+// Setup Express
+const app = express();
+secureExpressApp(app);
+
+const cosmosDbUri = getRequiredStringEnv("CUSTOMCONNSTR_COSMOSDB_URI");
+const cosmosDbKey = getRequiredStringEnv("CUSTOMCONNSTR_COSMOSDB_KEY");
+const cosmosDbName = getRequiredStringEnv("COSMOSDB_NAME");
+
+const documentDbDatabaseUrl = documentDbUtils.getDatabaseUri(cosmosDbName);
+const profilesCollectionUrl = documentDbUtils.getCollectionUri(
+  documentDbDatabaseUrl,
+  PROFILE_COLLECTION_NAME
+);
+
+const documentClient = new DocumentDBClient(cosmosDbUri, {
+  masterKey: cosmosDbKey
+});
+
+const profileModel = new ProfileModel(documentClient, profilesCollectionUrl);
+
+app.post(
+  "/adm/development-profiles/:fiscalcode",
+  CreateDevelopmentProfile(profileModel)
+);
+
+const azureFunctionHandler = createAzureFunctionHandler(app);
+
+// tslint:disable-next-line: no-let
+let logger: Context["log"] | undefined;
+const contextTransport = new AzureContextTransport(() => logger, {
+  level: "debug"
+});
+winston.add(contextTransport);
+
+// Binds the express app to an Azure Function handler
+function httpStart(context: Context): void {
+  logger = context.log;
+  setAppContext(app, context);
+  azureFunctionHandler(context);
+}
+
+export default httpStart;
