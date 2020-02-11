2e561a74382e16d5f001efdb292fa1233a1a78e8
alexgpeppe <alexgpeppe84@hotmail.it>
2020-02-11 10:58:18 +0100

    [#170129433] Add test suite for the GetServices functions

---
+++ GetServices/__tests__/handler.test.ts
@@ -0,0 +1,52 @@
+/* tslint:disable: no-any */
+
+import { left, right } from "fp-ts/lib/Either";
+
+import { aRetrievedService } from "../../__mocks__/mocks";
+import { GetServicesHandler } from "../handler";
+
+describe("GetService", () => {
+  it("Should return a query error when a database error occurs", async () => {
+    const mockServiceModel = {
+      getCollectionIterator: jest.fn(() => {
+        return Promise.resolve(left({}));
+      })
+    };
+
+    const getServiceHandler = GetServicesHandler(
+      undefined as any,
+      mockServiceModel as any
+    );
+
+    const response = await getServiceHandler(
+      undefined as any, // Not used
+      undefined as any // Not used
+    );
+
+    expect(mockServiceModel.getCollectionIterator).toHaveBeenCalledWith();
+    expect(response.kind).toBe("IResponseSuccessJsonIterator");
+  });
+
+  it("Should return the collection of services from the database", async () => {
+    const mockServiceModel = {
+      getCollectionIterator: jest.fn(() =>
+        Promise.resolve({
+          executeNext: () => Promise.resolve(right([aRetrievedService]))
+        })
+      )
+    };
+
+    const getServiceHandler = GetServicesHandler(
+      undefined as any,
+      mockServiceModel as any
+    );
+
+    const response = await getServiceHandler(
+      undefined as any, // Not used
+      undefined as any // Not used
+    );
+
+    expect(mockServiceModel.getCollectionIterator).toHaveBeenCalledWith();
+    expect(response.kind).toBe("IResponseSuccessJsonIterator");
+  });
+});
