6820da59caf48b5160dcc17ff7eba6fca7416c65
alexgpeppe <alexgpeppe84@hotmail.it>
2020-02-12 11:44:29 +0100

    [#170127598] Use the extended service model that includes metadata

--- utils/conversions.ts
+++ utils/conversions.ts
@@ -1,4 +1,5 @@
 import { Service as ApiService } from "io-functions-commons/dist/generated/definitions/Service";
+import { ServiceMetadata as ApiServiceMetadata } from "io-functions-commons/dist/generated/definitions/ServiceMetadata";
 import {
   RetrievedService,
   Service,
@@ -22,8 +23,42 @@ export function apiServiceToService(service: ApiService): Service {
     organizationName: service.organization_name,
     requireSecureChannels: service.require_secure_channels,
     serviceId: service.service_id,
+    serviceMetadata: service.service_metadata && {
+      address: service.service_metadata.address,
+      appAndroid: service.service_metadata.app_android,
+      appIos: service.service_metadata.app_ios,
+      description: service.service_metadata.description,
+      email: service.service_metadata.email,
+      pec: service.service_metadata.pec,
+      phone: service.service_metadata.phone,
+      privacyUrl: service.service_metadata.privacy_url,
+      scope: service.service_metadata.scope,
+      tosUrl: service.service_metadata.tos_url,
+      webUrl: service.service_metadata.web_url
+    },
     serviceName: service.service_name
   };
+}
+
+// Returns an API Service Metadata from an internal Service model
+function getApiServiceMetadataFromRetrievedService(
+  retrievedService: RetrievedService
+): ApiServiceMetadata {
+  return (
+    retrievedService.serviceMetadata && {
+      address: retrievedService.serviceMetadata.address,
+      app_android: retrievedService.serviceMetadata.appAndroid,
+      app_ios: retrievedService.serviceMetadata.appIos,
+      description: retrievedService.serviceMetadata.description,
+      email: retrievedService.serviceMetadata.email,
+      pec: retrievedService.serviceMetadata.pec,
+      phone: retrievedService.serviceMetadata.phone,
+      privacy_url: retrievedService.serviceMetadata.privacyUrl,
+      scope: retrievedService.serviceMetadata.scope,
+      tos_url: retrievedService.serviceMetadata.tosUrl,
+      web_url: retrievedService.serviceMetadata.webUrl
+    }
+  );
 }
 
 /**
@@ -47,6 +82,9 @@ export function retrievedServiceToApiService(
     organization_name: retrievedService.organizationName,
     require_secure_channels: retrievedService.requireSecureChannels,
     service_id: retrievedService.serviceId,
+    service_metadata: getApiServiceMetadataFromRetrievedService(
+      retrievedService
+    ),
     service_name: retrievedService.serviceName,
     version: retrievedService.version
   };
@@ -61,6 +99,9 @@ export function retrievedServiceToVisibleService(
     organizationFiscalCode: retrievedService.organizationFiscalCode,
     organizationName: retrievedService.organizationName,
     serviceId: retrievedService.serviceId,
+    serviceMetadata: getApiServiceMetadataFromRetrievedService(
+      retrievedService
+    ),
     serviceName: retrievedService.serviceName,
     version: retrievedService.version
   };
