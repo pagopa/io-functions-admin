2d5a10399e519195a6e0a6dc17b65a4058cccd49
danilo.spinelli <danilo.spinelli@pagopa.it>
2020-02-25 14:14:01 +0100

    [#171435619] get only latest services

--- GetServices/handler.ts
+++ GetServices/handler.ts
@@ -3,7 +3,11 @@ import { Context } from "@azure/functions";
 import * as express from "express";
 
 import { ServiceModel } from "io-functions-commons/dist/src/models/service";
-import { mapResultIterator } from "io-functions-commons/dist/src/utils/documentdb";
+import {
+  IFoldableResultIterator,
+  iteratorToValue,
+  reduceResultIterator
+} from "io-functions-commons/dist/src/utils/documentdb";
 import {
   AzureApiAuthMiddleware,
   IAzureApiAuthorization,
@@ -23,34 +27,63 @@ import {
   wrapRequestHandler
 } from "io-functions-commons/dist/src/utils/request_middleware";
 import {
-  IResponseSuccessJsonIterator,
-  ResponseJsonIterator
+  IResponseErrorQuery,
+  ResponseErrorQuery
 } from "io-functions-commons/dist/src/utils/response";
 import {
   checkSourceIpForHandler,
   clientIPAndCidrTuple as ipTuple
 } from "io-functions-commons/dist/src/utils/source_ip_check";
 
+import { collect, StrMap } from "fp-ts/lib/StrMap";
+import {
+  IResponseSuccessJson,
+  ResponseSuccessJson
+} from "italia-ts-commons/lib/responses";
 import { Service as ApiService } from "../generated/definitions/Service";
 import { retrievedServiceToApiService } from "../utils/conversions";
+
+type IGetServicesHandlerResult =
+  | IResponseErrorQuery
+  | IResponseSuccessJson<{ items: readonly ApiService[]; page_size: number }>;
 
 type IGetServicesHandler = (
   context: Context,
   auth: IAzureApiAuthorization,
   clientIp: ClientIp,
   userAttributes: IAzureUserAttributes
-) => Promise<IResponseSuccessJsonIterator<ApiService>>;
+) => Promise<IGetServicesHandlerResult>;
 
 export function GetServicesHandler(
   serviceModel: ServiceModel
 ): IGetServicesHandler {
   return async (_, __, ___, ____) => {
     const allRetrievedServicesIterator = await serviceModel.getCollectionIterator();
-    const allServicesIterator = mapResultIterator(
-      allRetrievedServicesIterator,
-      retrievedServiceToApiService
+    const allServicesIterator: IFoldableResultIterator<
+      Record<string, ApiService>
+    > = reduceResultIterator(allRetrievedServicesIterator, (prev, curr) => {
+      // keep only the latest version
+      const isNewer =
+        !prev[curr.serviceId] || curr.version > prev[curr.serviceId].version;
+      return {
+        ...prev,
+        ...(isNewer
+          ? { [curr.serviceId]: retrievedServiceToApiService(curr) }
+          : {})
+      };
+    });
+    return (await iteratorToValue(allServicesIterator, {})).fold<
+      IGetServicesHandlerResult
+    >(
+      error => ResponseErrorQuery("Cannot get services", error),
+      services => {
+        const items = collect(new StrMap(services), (_____, v) => v);
+        return ResponseSuccessJson({
+          items,
+          page_size: items.length
+        });
+      }
     );
-    return ResponseJsonIterator(allServicesIterator);
   };
 }
 
