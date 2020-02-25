d4f527dd4c2466f47ccfcb2b3aa58bf4fdc0ab85
Danilo Spinelli <gunzip@users.noreply.github.com>
2020-02-25 14:15:53 +0100

    [#171435619] get only latest services (#19)

--- GetServices/__tests__/handler.test.ts
+++ GetServices/__tests__/handler.test.ts
@@ -2,35 +2,16 @@
 
 import { left, right } from "fp-ts/lib/Either";
 
-import { aRetrievedService } from "../../__mocks__/mocks";
+import { none, some } from "fp-ts/lib/Option";
+import { aRetrievedService, aSeralizedService } from "../../__mocks__/mocks";
 import { GetServicesHandler } from "../handler";
 
 describe("GetServices", () => {
   it("Should return a query error when a database error occurs", async () => {
     const mockServiceModel = {
-      getCollectionIterator: jest.fn(() => {
-        return Promise.resolve(left({}));
-      })
-    };
-
-    const getServicesHandler = GetServicesHandler(mockServiceModel as any);
-
-    const response = await getServicesHandler(
-      undefined as any, // Not used
-      undefined as any, // Not used
-      undefined as any, // Not used
-      undefined as any // Not used
-    );
-
-    expect(mockServiceModel.getCollectionIterator).toHaveBeenCalledWith();
-    expect(response.kind).toBe("IResponseSuccessJsonIterator");
-  });
-
-  it("Should return the collection of services from the database", async () => {
-    const mockServiceModel = {
       getCollectionIterator: jest.fn(() =>
         Promise.resolve({
-          executeNext: () => Promise.resolve(right([aRetrievedService]))
+          executeNext: () => Promise.resolve(left(new Error()))
         })
       )
     };
@@ -45,6 +26,59 @@ describe("GetServices", () => {
     );
 
     expect(mockServiceModel.getCollectionIterator).toHaveBeenCalledWith();
-    expect(response.kind).toBe("IResponseSuccessJsonIterator");
+    expect(response.kind).toBe("IResponseErrorQuery");
+  });
+
+  it("Should return the collection of services from the database", async () => {
+    const mockServiceModel = {
+      getCollectionIterator: jest.fn(() =>
+        Promise.resolve({
+          executeNext: jest
+            .fn()
+            .mockImplementationOnce(() =>
+              Promise.resolve(
+                right(
+                  some([
+                    aRetrievedService,
+                    {
+                      ...aRetrievedService,
+                      version: 3
+                    },
+                    {
+                      ...aRetrievedService,
+                      version: 2
+                    }
+                  ])
+                )
+              )
+            )
+            .mockImplementationOnce(() => Promise.resolve(right(none)))
+        })
+      )
+    };
+
+    const getServicesHandler = GetServicesHandler(mockServiceModel as any);
+
+    const response = await getServicesHandler(
+      undefined as any, // Not used
+      undefined as any, // Not used
+      undefined as any, // Not used
+      undefined as any // Not used
+    );
+
+    expect(mockServiceModel.getCollectionIterator).toHaveBeenCalledWith();
+    expect(response.kind).toBe("IResponseSuccessJson");
+    if (response.kind === "IResponseSuccessJson") {
+      expect(response.value).toEqual({
+        items: [
+          {
+            ...aSeralizedService,
+            service_metadata: undefined,
+            version: 3
+          }
+        ],
+        page_size: 1
+      });
+    }
   });
 });
