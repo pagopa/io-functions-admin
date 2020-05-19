c7fa83374d4191d734ddb8a71a58b5829f498d5f
Emanuele De Cupis <ema.decu@gmail.com>
2020-05-19 11:00:19 +0200

    setting user data processing status

--- UserDataDeleteOrchestrator/cli.ts
+++ UserDataDeleteOrchestrator/cli.ts
@@ -11,13 +11,20 @@ import { readableReport } from "italia-ts-commons/lib/reporters";
 import { FiscalCode } from "italia-ts-commons/lib/strings";
 import RedisSessionStorage from "./session-utils/redisSessionStorage";
 
+import GetUserDataProcessingActivity from "../GetUserDataProcessingActivity";
+import SetUserDataProcessingStatusActivity from "../SetUserDataProcessingStatusActivity";
+
 import { sequenceT } from "fp-ts/lib/Apply";
 import { Either, toError } from "fp-ts/lib/Either";
 import { taskEither, TaskEither, tryCatch } from "fp-ts/lib/TaskEither";
+import { UserDataProcessingChoiceEnum } from "io-functions-commons/dist/generated/definitions/UserDataProcessingChoice";
 import { UserDataProcessingStatusEnum } from "io-functions-commons/dist/generated/definitions/UserDataProcessingStatus";
+import { UserDataProcessing } from "io-functions-commons/dist/src/models/user_data_processing";
 import { getRequiredStringEnv } from "io-functions-commons/dist/src/utils/env";
 import { NodeEnvironmentEnum } from "italia-ts-commons/lib/environment";
 import { getNodeEnvironmentFromProcessEnv } from "italia-ts-commons/lib/environment";
+
+import { Context } from "@azure/functions";
 import {
   createClusterRedisClient,
   createSimpleRedisClient
@@ -26,6 +33,16 @@ const trace = (l: string) => (e: any) => {
   console.log(l, e);
   return e;
 };
+
+const context = ({
+  log: {
+    error: console.error,
+    info: console.log,
+    verbose: console.log
+  }
+  // tslint:disable-next-line: no-any
+} as any) as Context;
+
 const REDIS_CLIENT =
   getNodeEnvironmentFromProcessEnv(process.env) ===
   NodeEnvironmentEnum.DEVELOPMENT
@@ -102,9 +119,24 @@ const deleteUserData = (
 
 // change status on user request
 const setUserDataProcessingStatus = (
-  // tslint:disable-next-line: variable-name
-  _status: UserDataProcessingStatusEnum
-): TaskEither<Error, true> => notImplementedTask("setUserDataProcessingStatus");
+  currentUserDataProcessing: UserDataProcessing,
+  nextStatus: UserDataProcessingStatusEnum
+): TaskEither<Error, true> =>
+  tryCatch(
+    () =>
+      SetUserDataProcessingStatusActivity(context, {
+        currentRecord: currentUserDataProcessing,
+        nextStatus
+      }).then(result => {
+        if (result.kind !== "SUCCESS") {
+          throw new Error(
+            `SetUserDataProcessingStatusActivity to ${nextStatus} failed: ${result.kind} error`
+          );
+        }
+        return true;
+      }),
+    toError
+  );
 
 // after the operation, unblock the user to allow another login
 const unblockUser = (fiscalCode: FiscalCode): TaskEither<Error, true> =>
@@ -125,13 +157,59 @@ async function run(): Promise<Either<Error, boolean>> {
     throw new Error(`Invalid input: ${readableReport(reason)}`);
   });
 
+  const userDataProcessingResult = await GetUserDataProcessingActivity(
+    context,
+    {
+      choice: UserDataProcessingChoiceEnum.DELETE,
+      fiscalCode
+    }
+  );
+
+  if (userDataProcessingResult.kind === "RECORD_NOT_FOUND") {
+    throw new Error(`No data delete has been requested for the current user`);
+  } else if (userDataProcessingResult.kind !== "SUCCESS") {
+    throw new Error("Failed retrieving userDataProcessing");
+  } else if (
+    userDataProcessingResult.value.status !==
+      UserDataProcessingStatusEnum.PENDING &&
+    userDataProcessingResult.value.status !==
+      UserDataProcessingStatusEnum.FAILED
+  ) {
+    throw new Error("User data processing status !== PENDING & != FAILED");
+  } else {
+    console.log(
+      "Found user data processing request with status %s",
+      userDataProcessingResult.value.status
+    );
+  }
+
   return blockUser(fiscalCode)
-    .chain(_ => setUserDataProcessingStatus(UserDataProcessingStatusEnum.WIP))
+    .chain(_ =>
+      setUserDataProcessingStatus(
+        userDataProcessingResult.value,
+        UserDataProcessingStatusEnum.WIP
+      )
+    )
     .chain(_ => saveUserDataToStorage(fiscalCode))
     .chain(_ => deleteUserData(fiscalCode))
     .chain(_ => unblockUser(fiscalCode))
     .chain(_ =>
-      setUserDataProcessingStatus(UserDataProcessingStatusEnum.CLOSED)
+      setUserDataProcessingStatus(
+        userDataProcessingResult.value,
+        UserDataProcessingStatusEnum.CLOSED
+      )
+    )
+    .foldTaskEither(
+      _ => {
+        console.log("Something went wrong. Mark the requeste as FAILED");
+        // mark as failed
+        return setUserDataProcessingStatus(
+          userDataProcessingResult.value,
+          UserDataProcessingStatusEnum.FAILED
+        );
+      },
+      // just pass
+      e => taskEither.of(e)
     )
     .run();
 }
