9bea937c0c8f66ea2944be421753de520bf334ea
Danilo Spinelli <gunzip@users.noreply.github.com>
2020-07-16 16:42:48 +0200

    Add info endpoint (#61)

---
+++ Info/index.ts
@@ -0,0 +1,22 @@
+import { AzureFunction, Context } from "@azure/functions";
+import * as express from "express";
+import { secureExpressApp } from "io-functions-commons/dist/src/utils/express";
+import { setAppContext } from "io-functions-commons/dist/src/utils/middlewares/context_middleware";
+import createAzureFunctionHandler from "io-functions-express/dist/src/createAzureFunctionsHandler";
+import { Info } from "./handler";
+
+// Setup Express
+const app = express();
+secureExpressApp(app);
+
+// Add express route
+app.get("/adm/info", Info());
+
+const azureFunctionHandler = createAzureFunctionHandler(app);
+
+const httpStart: AzureFunction = (context: Context): void => {
+  setAppContext(app, context);
+  azureFunctionHandler(context);
+};
+
+export default httpStart;
