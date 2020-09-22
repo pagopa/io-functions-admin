9b363abf46b1a02e271538ba2fc93f8723545f81
Emanuele De Cupis <ema.decu@gmail.com>
2020-09-22 11:01:35 +0200

    [#174710415] Send confirmation email to user after data delete (#86)

---
+++ GetProfileActivity/__tests__/handler.test.ts
@@ -0,0 +1,97 @@
+/* tslint:disable: no-any no-identical-functions */
+
+import { right } from "fp-ts/lib/Either";
+
+import { context as contextMock } from "../../__mocks__/durable-functions";
+import { aFiscalCode, aRetrievedProfile } from "../../__mocks__/mocks";
+
+import {
+  ActivityInput,
+  ActivityResultInvalidInputFailure,
+  ActivityResultNotFoundFailure,
+  ActivityResultQueryFailure,
+  ActivityResultSuccess,
+  createGetProfileActivityHandler
+} from "../handler";
+
+import { none, some } from "fp-ts/lib/Option";
+import { fromEither, fromLeft } from "fp-ts/lib/TaskEither";
+import { toCosmosErrorResponse } from "io-functions-commons/dist/src/utils/cosmosdb_model";
+import { ProfileModel } from "io-functions-commons/dist/src/models/profile";
+
+describe("GetProfileActivityHandler", () => {
+  it("should handle a result", async () => {
+    const mockModel = ({
+      findLastVersionByModelId: jest.fn(() =>
+        fromEither(right(some(aRetrievedProfile)))
+      )
+    } as any) as ProfileModel;
+
+    const handler = createGetProfileActivityHandler(mockModel);
+    const input: ActivityInput = {
+      fiscalCode: aFiscalCode
+    };
+    const result = await handler(contextMock, input);
+
+    expect(ActivityResultSuccess.decode(result).isRight()).toBe(true);
+  });
+
+  it("should handle a record not found failure", async () => {
+    const mockModel = ({
+      findLastVersionByModelId: jest.fn(() => fromEither(right(none)))
+    } as any) as ProfileModel;
+
+    const handler = createGetProfileActivityHandler(mockModel);
+    const input: ActivityInput = {
+      fiscalCode: aFiscalCode
+    };
+    const result = await handler(contextMock, input);
+
+    expect(ActivityResultNotFoundFailure.decode(result).isRight()).toBe(true);
+  });
+
+  it("should handle a query error", async () => {
+    const mockModel = ({
+      findLastVersionByModelId: jest.fn(() =>
+        fromLeft(toCosmosErrorResponse({ kind: "COSMOS_ERROR_RESPONSE" }))
+      )
+    } as any) as ProfileModel;
+
+    const handler = createGetProfileActivityHandler(mockModel);
+    const input: ActivityInput = {
+      fiscalCode: aFiscalCode
+    };
+    const result = await handler(contextMock, input);
+
+    expect(ActivityResultQueryFailure.decode(result).isRight()).toBe(true);
+  });
+
+  it("should handle a rejection", async () => {
+    const mockModel = ({
+      findLastVersionByModelId: jest.fn(() => fromEither(right(none)))
+    } as any) as ProfileModel;
+
+    const handler = createGetProfileActivityHandler(mockModel);
+    const input: ActivityInput = {
+      fiscalCode: aFiscalCode
+    };
+    const result = await handler(contextMock, input);
+
+    expect(ActivityResultNotFoundFailure.decode(result).isRight()).toBe(true);
+  });
+
+  it("should handle an invalid input", async () => {
+    const mockModel = ({} as any) as ProfileModel;
+
+    const handler = createGetProfileActivityHandler(mockModel);
+
+    // @ts-ignore to force bad behavior
+    const result = await handler(contextMock, {
+      invalid: "input"
+    });
+
+    expect(ActivityResultInvalidInputFailure.decode(result).isRight()).toBe(
+      true
+    );
+  });
+});
---
+++ utils/email.ts
@@ -0,0 +1,112 @@
+import { catOptions } from "fp-ts/lib/Array";
+import { none, some } from "fp-ts/lib/Option";
+
+import { MailUpTransport } from "io-functions-commons/dist/src/utils/mailup";
+import { MailMultiTransportConnections } from "io-functions-commons/dist/src/utils/multi_transport_connection";
+
+import { agent } from "italia-ts-commons";
+import {
+  AbortableFetch,
+  setFetchTimeout,
+  toFetch
+} from "italia-ts-commons/lib/fetch";
+import { Millisecond } from "italia-ts-commons/lib/units";
+
+import { NonEmptyString } from "italia-ts-commons/lib/strings";
+
+import * as NodeMailer from "nodemailer";
+import nodemailerSendgrid = require("nodemailer-sendgrid");
+import Mail = require("nodemailer/lib/mailer");
+
+// 5 seconds timeout by default
+const DEFAULT_EMAIL_REQUEST_TIMEOUT_MS = 5000;
+
+// Must be an https endpoint so we use an https agent
+const abortableFetch = AbortableFetch(agent.getHttpsFetch(process.env));
+const fetchWithTimeout = setFetchTimeout(
+  DEFAULT_EMAIL_REQUEST_TIMEOUT_MS as Millisecond,
+  abortableFetch
+);
+
+interface IMailUpOptions {
+  mailupSecret: NonEmptyString;
+  mailupUsername: NonEmptyString;
+}
+
+interface ISendGridOptions {
+  sendgridApiKey: NonEmptyString;
+}
+
+type MailTransportOptions = (IMailUpOptions | ISendGridOptions) & {
+  isProduction: boolean;
+};
+
+export function getMailerTransporter(opts: MailTransportOptions): Mail {
+  return opts.isProduction
+    ? NodeMailer.createTransport(
+        "sendgridApiKey" in opts
+          ? nodemailerSendgrid({
+              apiKey: opts.sendgridApiKey
+            })
+          : MailUpTransport({
+              creds: {
+                Secret: opts.mailupSecret,
+                Username: opts.mailupUsername
+              },
+              // HTTPS-only fetch with optional keepalive agent
+              fetchAgent: toFetch(fetchWithTimeout)
+            })
+      )
+    : // For development we use mailhog to intercept emails
+      // Use the `docker-compose.yml` file to run the mailhog server
+      NodeMailer.createTransport({
+        host: process.env.MAILHOG_HOSTNAME || "localhost",
+        port: 1025,
+        secure: false
+      });
+}
+
+/**
+ * Converts an array of mail transport connections into their corresponding
+ * nodemailer transports
+ */
+export function getTransportsForConnections(
+  configs: MailMultiTransportConnections
+): ReadonlyArray<NodeMailer.Transport> {
+  return catOptions(
+    configs.map(config => {
+      // configure mailup
+      if (
+        config.transport === "mailup" &&
+        NonEmptyString.is(config.password) &&
+        NonEmptyString.is(config.username)
+      ) {
+        return some(
+          MailUpTransport({
+            creds: {
+              Secret: config.password,
+              Username: config.username
+            },
+            // HTTPS-only fetch with optional keepalive agent
+            fetchAgent: toFetch(fetchWithTimeout)
+          })
+        );
+      }
+
+      // sendgrid uses username as api key
+      if (
+        config.transport === "sendgrid" &&
+        NonEmptyString.is(config.username)
+      ) {
+        return some(
+          nodemailerSendgrid({
+            apiKey: config.username
+          })
+        );
+      }
+
+      // default ignore
+      return none;
+    })
+  );
+}
