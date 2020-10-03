8433210280884dee8ad159bf638c50ea80ad39f0
Emanuele De Cupis <ema.decu@gmail.com>
2020-10-03 16:02:08 +0200

    sync config

---
+++ utils/__tests__/config.test.ts
@@ -0,0 +1,199 @@
+import { Either } from "fp-ts/lib/Either";
+import { NonEmptyString } from "italia-ts-commons/lib/strings";
+import { MailerConfig } from "../config";
+
+const aMailFrom = "example@test.com";
+
+const noop = () => {};
+const expectRight = <L, R>(e: Either<L, R>, t: (r: R) => void = noop) =>
+  e.fold(
+    _ =>
+      fail(`Expecting right, received left. Value: ${JSON.stringify(e.value)}`),
+    _ => t(_)
+  );
+
+const expectLeft = <L, R>(e: Either<L, R>, t: (l: L) => void = noop) =>
+  e.fold(
+    _ => t(_),
+    _ =>
+      fail(`Expecting left, received right. Value: ${JSON.stringify(e.value)}`)
+  );
+
+describe("MailerConfig", () => {
+  it("should decode configuration for sendgrid", () => {
+    const rawConf = {
+      MAIL_FROM: aMailFrom,
+      NODE_ENV: "production",
+      SENDGRID_API_KEY: "a-sg-key"
+    };
+    const result = MailerConfig.decode(rawConf);
+
+    expectRight(result, value => {
+      expect(value.SENDGRID_API_KEY).toBe("a-sg-key");
+      expect(typeof value.MAILUP_USERNAME).toBe("undefined");
+    });
+  });
+
+  it("should decode configuration for sendgrid", () => {
+    const rawConf = {
+      MAIL_FROM: aMailFrom,
+      NODE_ENV: "production",
+      SENDGRID_API_KEY: "a-sg-key"
+    };
+    const result = MailerConfig.decode(rawConf);
+
+    expectRight(result, value => {
+      expect(value.SENDGRID_API_KEY).toBe("a-sg-key");
+      expect(typeof value.MAILUP_USERNAME).toBe("undefined");
+    });
+  });
+
+  it("should decode configuration for sendgrid even if mailup conf is passed", () => {
+    const rawConf = {
+      MAIL_FROM: aMailFrom,
+      NODE_ENV: "production",
+      SENDGRID_API_KEY: "a-sg-key",
+      MAILUP_USERNAME: "a-mu-username",
+      MAILUP_SECRET: "a-mu-secret"
+    };
+    const result = MailerConfig.decode(rawConf);
+
+    expectRight(result, value => {
+      expect(value.SENDGRID_API_KEY).toBe("a-sg-key");
+    });
+  });
+
+  it("should decode configuration for mailup", () => {
+    const rawConf = {
+      MAIL_FROM: aMailFrom,
+      NODE_ENV: "production",
+      MAILUP_USERNAME: "a-mu-username",
+      MAILUP_SECRET: "a-mu-secret"
+    };
+    const result = MailerConfig.decode(rawConf);
+
+    expectRight(result, value => {
+      expect(value.MAILUP_USERNAME).toBe("a-mu-username");
+      expect(value.MAILUP_SECRET).toBe("a-mu-secret");
+      // check types
+      const _: NonEmptyString = value.MAILUP_SECRET;
+      const __: NonEmptyString = value.MAILUP_USERNAME;
+    });
+  });
+
+  it("should decode configuration with multi transport", () => {
+    const aTransport = {
+      password: "abc".repeat(5),
+      transport: "transport-name",
+      username: "t-username"
+    };
+    const aRawTrasport = [
+      aTransport.transport,
+      aTransport.username,
+      aTransport.password
+    ].join(":");
+
+    const rawConf = {
+      MAIL_FROM: aMailFrom,
+      NODE_ENV: "production",
+      MAIL_TRANSPORTS: [aRawTrasport, aRawTrasport].join(";")
+    };
+    const result = MailerConfig.decode(rawConf);
+
+    expectRight(result, value => {
+      expect(value.MAIL_TRANSPORTS).toEqual([aTransport, aTransport]);
+    });
+  });
+
+  it("should decode configuration for mailhog", () => {
+    const rawConf = {
+      MAIL_FROM: aMailFrom,
+      NODE_ENV: "dev",
+      MAILHOG_HOSTNAME: "a-mh-host"
+    };
+    const result = MailerConfig.decode(rawConf);
+
+    expectRight(result, value => {
+      expect(value.MAILHOG_HOSTNAME).toBe("a-mh-host");
+    });
+  });
+
+  it("should require mailhog if not in prod", () => {
+    const rawConf = {
+      MAIL_FROM: aMailFrom,
+      NODE_ENV: "dev"
+    };
+    const result = MailerConfig.decode(rawConf);
+
+    expectLeft(result);
+  });
+
+  it("should require at least on transporter if in prod", () => {
+    const rawConf = {
+      MAIL_FROM: aMailFrom,
+      NODE_ENV: "production"
+    };
+    const result = MailerConfig.decode(rawConf);
+
+    expectLeft(result);
+  });
+
+  it("should not allow mailhog if in prod", () => {
+    const rawConf = {
+      MAIL_FROM: aMailFrom,
+      NODE_ENV: "production",
+      MAILHOG_HOSTNAME: "a-mh-host"
+    };
+    const result = MailerConfig.decode(rawConf);
+
+    expectLeft(result);
+  });
+
+  it("should not decode configuration with empty transport", () => {
+    const rawConf = {
+      MAIL_FROM: aMailFrom,
+      NODE_ENV: "production",
+      MAIL_TRANSPORTS: ""
+    };
+    const result = MailerConfig.decode(rawConf);
+
+    expectLeft(result);
+  });
+
+  it("should not decode configuration when no transporter is specified", () => {
+    const rawConf = {
+      MAIL_FROM: aMailFrom
+    };
+    const result = MailerConfig.decode(rawConf);
+
+    expectLeft(result);
+  });
+
+  it("should not decode ambiguos configuration", () => {
+    const withMailUp = {
+      MAILUP_USERNAME: "a-mu-username",
+      MAILUP_SECRET: "a-mu-secret"
+    };
+    const withSendGrid = {
+      SENDGRID_API_KEY: "a-sg-key"
+    };
+    const withMultiTransport = {
+      MAIL_TRANSPORTS: "a-trasnport-name"
+    };
+    const base = {
+      MAIL_FROM: aMailFrom,
+      NODE_ENV: "production"
+    };
+
+    const examples = [
+      // the following configuration is not ambiguos as sendgrid would override mailup anyway
+      // see here for the rationale: https://github.com/pagopa/io-functions-admin/pull/89#commitcomment-42917672
+      // { ...base, ...withMailUp, ...withSendGrid },
+      { ...base, ...withMultiTransport, ...withSendGrid },
+      { ...base, ...withMailUp, ...withMultiTransport },
+      { ...base, ...withMailUp, ...withSendGrid, ...withMultiTransport }
+    ];
+
+    examples.map(MailerConfig.decode).forEach(_ => expectLeft(_));
+  });
+});
--- utils/config.ts
+++ utils/config.ts
@@ -5,65 +5,136 @@
  * The configuration is evaluate eagerly at the first access to the module. The module exposes convenient methods to access such value.
  */
 
+import { MailMultiTransportConnectionsFromString } from "io-functions-commons/dist/src/utils/multi_transport_connection";
 import * as t from "io-ts";
 import { readableReport } from "italia-ts-commons/lib/reporters";
 import { NonEmptyString } from "italia-ts-commons/lib/strings";
 
-const NullableString = t.union([t.string, t.undefined]);
+// exclude a specific value from a type
+// as strict equality is performed, allowed input types are constrained to be values not references (object, arrays, etc)
+// tslint:disable-next-line max-union-size
+const AnyBut = <A extends string | number | boolean | symbol, O = A>(
+  but: A,
+  base: t.Type<A, O> = t.any
+) =>
+  t.brand(
+    base,
+    (
+      s
+    ): s is t.Branded<
+      t.TypeOf<typeof base>,
+      { readonly AnyBut: unique symbol }
+    > => s !== but,
+    "AnyBut"
+  );
 
+// configuration to send email
+export type MailerConfig = t.TypeOf<typeof MailerConfig>;
+export const MailerConfig = t.intersection([
+  // common required fields
+  t.interface({
+    MAIL_FROM: NonEmptyString
+  }),
+  // the following union includes the possible configuration variants for different mail transports we use in prod
+  // undefined values are kept for easy usage
+  t.union([
+    // Using sendgrid
+    // we allow mailup values as well, as sendgrid would be selected first if present
+    // see here for the rationale: https://github.com/pagopa/io-functions-admin/pull/89#commitcomment-42917672
+    t.intersection([
+      t.interface({
+        MAILHOG_HOSTNAME: t.undefined,
+        MAIL_TRANSPORTS: t.undefined,
+        NODE_ENV: t.literal("production"),
+        SENDGRID_API_KEY: NonEmptyString
+      }),
+      t.partial({
+        MAILUP_SECRET: NonEmptyString,
+        MAILUP_USERNAME: NonEmptyString
+      })
+    ]),
+    // using mailup
+    t.interface({
+      MAILHOG_HOSTNAME: t.undefined,
+      MAILUP_SECRET: NonEmptyString,
+      MAILUP_USERNAME: NonEmptyString,
+      MAIL_TRANSPORTS: t.undefined,
+      NODE_ENV: t.literal("production"),
+      SENDGRID_API_KEY: t.undefined
+    }),
+    // Using multi-transport definition
+    // Optional multi provider connection string
+    // The connection string must be in the format:
+    //   [mailup:username:password;][sendgrid:apikey:;]
+    // Note that multiple instances of the same provider can be provided.
+    t.interface({
+      MAILHOG_HOSTNAME: t.undefined,
+      MAILUP_SECRET: t.undefined,
+      MAILUP_USERNAME: t.undefined,
+      MAIL_TRANSPORTS: MailMultiTransportConnectionsFromString,
+      NODE_ENV: t.literal("production"),
+      SENDGRID_API_KEY: t.undefined
+    }),
+    // the following states that a mailhog configuration is optional and can be provided only if not in prod
+    t.interface({
+      MAILHOG_HOSTNAME: NonEmptyString,
+      MAILUP_SECRET: t.undefined,
+      MAILUP_USERNAME: t.undefined,
+      MAIL_TRANSPORTS: t.undefined,
+      NODE_ENV: AnyBut("production", t.string),
+      SENDGRID_API_KEY: t.undefined
+    })
+  ])
+]);
+
+// global app configuration
 export type IConfig = t.TypeOf<typeof IConfig>;
-export const IConfig = t.interface({
-  COSMOSDB_KEY: NonEmptyString,
-  COSMOSDB_NAME: NonEmptyString,
-  COSMOSDB_URI: NonEmptyString,
+export const IConfig = t.intersection([
+  t.interface({
+    COSMOSDB_KEY: NonEmptyString,
+    COSMOSDB_NAME: NonEmptyString,
+    COSMOSDB_URI: NonEmptyString,
 
-  SERVICE_PRINCIPAL_CLIENT_ID: NonEmptyString,
-  SERVICE_PRINCIPAL_SECRET: NonEmptyString,
-  SERVICE_PRINCIPAL_TENANT_ID: NonEmptyString,
+    SERVICE_PRINCIPAL_CLIENT_ID: NonEmptyString,
+    SERVICE_PRINCIPAL_SECRET: NonEmptyString,
+    SERVICE_PRINCIPAL_TENANT_ID: NonEmptyString,
 
-  AZURE_APIM: NonEmptyString,
-  AZURE_APIM_HOST: NonEmptyString,
-  AZURE_APIM_RESOURCE_GROUP: NonEmptyString,
-  AZURE_SUBSCRIPTION_ID: NonEmptyString,
+    AZURE_APIM: NonEmptyString,
+    AZURE_APIM_HOST: NonEmptyString,
+    AZURE_APIM_RESOURCE_GROUP: NonEmptyString,
+    AZURE_SUBSCRIPTION_ID: NonEmptyString,
 
-  ADB2C_CLIENT_ID: NonEmptyString,
-  ADB2C_CLIENT_KEY: NonEmptyString,
-  ADB2C_TENANT_ID: NonEmptyString,
+    ADB2C_CLIENT_ID: NonEmptyString,
+    ADB2C_CLIENT_KEY: NonEmptyString,
+    ADB2C_TENANT_ID: NonEmptyString,
 
-  UserDataBackupStorageConnection: NonEmptyString,
+    UserDataBackupStorageConnection: NonEmptyString,
 
-  MESSAGE_CONTAINER_NAME: NonEmptyString,
-  USER_DATA_BACKUP_CONTAINER_NAME: NonEmptyString,
-  USER_DATA_CONTAINER_NAME: NonEmptyString,
+    MESSAGE_CONTAINER_NAME: NonEmptyString,
+    USER_DATA_BACKUP_CONTAINER_NAME: NonEmptyString,
+    USER_DATA_CONTAINER_NAME: NonEmptyString,
 
-  StorageConnection: NonEmptyString,
-  SubscriptionFeedStorageConnection: NonEmptyString,
-  UserDataArchiveStorageConnection: NonEmptyString,
+    StorageConnection: NonEmptyString,
+    SubscriptionFeedStorageConnection: NonEmptyString,
+    UserDataArchiveStorageConnection: NonEmptyString,
 
-  PUBLIC_API_KEY: NonEmptyString,
-  PUBLIC_API_URL: NonEmptyString,
+    PUBLIC_API_KEY: NonEmptyString,
+    PUBLIC_API_URL: NonEmptyString,
 
-  PUBLIC_DOWNLOAD_BASE_URL: NonEmptyString,
+    PUBLIC_DOWNLOAD_BASE_URL: NonEmptyString,
 
-  SESSION_API_KEY: NonEmptyString,
-  SESSION_API_URL: NonEmptyString,
+    SESSION_API_KEY: NonEmptyString,
+    SESSION_API_URL: NonEmptyString,
 
-  LOGOS_URL: NonEmptyString,
+    LOGOS_URL: NonEmptyString,
 
-  SUBSCRIPTIONS_FEED_TABLE: NonEmptyString,
-  USER_DATA_DELETE_DELAY_DAYS: NonEmptyString,
+    SUBSCRIPTIONS_FEED_TABLE: NonEmptyString,
+    USER_DATA_DELETE_DELAY_DAYS: NonEmptyString,
 
-  // FIXME: email configuration values may be required or not depending on their values
-  MAIL_FROM: NonEmptyString,
-  MAIL_TRANSPORTS: NullableString,
-
-  MAILHOG_HOSTNAME: NullableString,
-  MAILUP_SECRET: NullableString,
-  MAILUP_USERNAME: NullableString,
-  SENDGRID_API_KEY: NullableString,
-
-  isProduction: t.boolean
-});
+    isProduction: t.boolean
+  }),
+  MailerConfig
+]);
 
 // No need to re-evaluate this object for each call
 const errorOrConfig: t.Validation<IConfig> = IConfig.decode({
