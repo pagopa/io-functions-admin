8ae4aefb964779ecc767982ff63874033cbf2a73
Alessio Dore <57567806+AleDore@users.noreply.github.com>
2020-10-22 14:18:55 +0200

    [#175374436] Allow lowercase transformation on png naming while uploading a Service image Logo (#95)
    * [#175374436] Png naming to lowercase on upload
    
    * [#175374436] Fix function.json
    
    * [#175374436] FIx lint
    
    * [#175374436] Add AssetsStorageConnection in config
    
    * [#175374436] Refactor over review
    
    * [#175374436] Refactor over integration tests

--- UploadServiceLogo/handler.ts
+++ UploadServiceLogo/handler.ts
@@ -2,14 +2,15 @@ import { Context } from "@azure/functions";
 
 import * as express from "express";
 
-import { isLeft } from "fp-ts/lib/Either";
-import { fromPredicate, isNone } from "fp-ts/lib/Option";
+import { isLeft, left, right } from "fp-ts/lib/Either";
+import { fromNullable, isNone } from "fp-ts/lib/Option";
 
 import {
   IResponseErrorInternal,
   IResponseErrorNotFound,
   IResponseErrorValidation,
   IResponseSuccessRedirectToResource,
+  ResponseErrorInternal,
   ResponseErrorNotFound,
   ResponseErrorValidation,
   ResponseSuccessRedirectToResource
@@ -31,7 +32,17 @@ import {
   ResponseErrorQuery
 } from "io-functions-commons/dist/src/utils/response";
 
-import { tryCatch } from "fp-ts/lib/Option";
+import { BlobService } from "azure-storage";
+import { identity } from "fp-ts/lib/function";
+import { Option, tryCatch } from "fp-ts/lib/Option";
+import {
+  fromLeft,
+  fromPredicate as fromPredicateT,
+  taskEither,
+  TaskEither,
+  taskify
+} from "fp-ts/lib/TaskEither";
+import { fromEither } from "fp-ts/lib/TaskEither";
 import * as UPNG from "upng-js";
 import { Logo as ApiLogo } from "../generated/definitions/Logo";
 import { ServiceId } from "../generated/definitions/ServiceId";
@@ -58,11 +69,23 @@ const imageValidationErrorResponse = () =>
     "The base64 representation of the logo is invalid"
   );
 
+const upsertBlobFromImageBuffer = (
+  blobService: BlobService,
+  containerName: string,
+  blobName: string,
+  content: Buffer
+): TaskEither<Error, Option<BlobService.BlobResult>> => {
+  return taskify<Error, BlobService.BlobResult>(cb =>
+    blobService.createBlockBlobFromText(containerName, blobName, content, cb)
+  )().map(fromNullable);
+};
+
 export function UpdateServiceLogoHandler(
   serviceModel: ServiceModel,
+  blobService: BlobService,
   logosUrl: string
 ): IUpdateServiceHandler {
-  return async (context, _, serviceId, logoPayload) => {
+  return async (_, __, serviceId, logoPayload) => {
     const errorOrMaybeRetrievedService = await serviceModel
       .findOneByServiceId(serviceId)
       .run();
@@ -82,27 +105,64 @@ export function UpdateServiceLogoHandler(
     }
 
     const bufferImage = Buffer.from(logoPayload.logo, "base64");
-    return tryCatch(() => UPNG.decode(bufferImage)).foldL(
-      () => imageValidationErrorResponse(),
-      image =>
-        fromPredicate((img: UPNG.Image) => img.width > 0 && img.height > 0)(
-          image
-        ).foldL<
-          IResponseErrorValidation | IResponseSuccessRedirectToResource<{}, {}>
-        >(
-          () => imageValidationErrorResponse(),
-          () => {
-            // tslint:disable-next-line:no-object-mutation
-            context.bindings.logo = bufferImage;
-
-            return ResponseSuccessRedirectToResource(
-              {},
-              `${logosUrl}/services/${serviceId}.png`,
-              {}
-            );
-          }
-        )
-    );
+    const lowerCaseServiceId = serviceId.toLowerCase();
+    return fromEither(
+      tryCatch(() => UPNG.decode(bufferImage)).foldL(
+        () =>
+          left<IResponseErrorValidation, UPNG.Image>(
+            imageValidationErrorResponse()
+          ),
+        img => right<IResponseErrorValidation, UPNG.Image>(img)
+      )
+    )
+      .chain(image =>
+        fromPredicateT(
+          (img: UPNG.Image) => img.width > 0 && img.height > 0,
+          () => imageValidationErrorResponse()
+        )(image)
+      )
+      .foldTaskEither<
+        IResponseErrorValidation | IResponseErrorInternal,
+        IResponseSuccessRedirectToResource<{}, {}>
+      >(
+        imageValidationError => fromLeft(imageValidationError),
+        () =>
+          upsertBlobFromImageBuffer(
+            blobService,
+            "services",
+            `${lowerCaseServiceId}.png`,
+            bufferImage
+          )
+            .mapLeft(err =>
+              ResponseErrorInternal(
+                `Error trying to connect to storage ${err.message}`
+              )
+            )
+            .chain(maybeResult =>
+              maybeResult.foldL(
+                () =>
+                  fromLeft(
+                    ResponseErrorInternal(
+                      "Error trying to upload image logo on storage"
+                    )
+                  ),
+                () =>
+                  taskEither.of(
+                    ResponseSuccessRedirectToResource(
+                      {},
+                      `${logosUrl}/services/${lowerCaseServiceId}.png`,
+                      {}
+                    )
+                  )
+              )
+            )
+      )
+      .fold<
+        | IResponseErrorValidation
+        | IResponseErrorInternal
+        | IResponseSuccessRedirectToResource<{}, {}>
+      >(identity, identity)
+      .run();
   };
 }
 
@@ -111,9 +171,10 @@ export function UpdateServiceLogoHandler(
  */
 export function UploadServiceLogo(
   serviceModel: ServiceModel,
+  blobService: BlobService,
   logosUrl: string
 ): express.RequestHandler {
-  const handler = UpdateServiceLogoHandler(serviceModel, logosUrl);
+  const handler = UpdateServiceLogoHandler(serviceModel, blobService, logosUrl);
 
   const middlewaresWrap = withRequestMiddlewares(
     // Extract Azure Functions bindings
