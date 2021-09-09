4d7a4824d0a73673c2e5c560488b5e581020e46d
Emanuele De Cupis <ema.decu@gmail.com>
2021-09-09 13:46:41 +0200

    [#ip-387] update UpdateVisibleServicesCache

---
+++ UpdateVisibleServicesCache/__test__/index.test.ts
@@ -0,0 +1,48 @@
+﻿import { Context } from "@azure/functions";
+import { index as updateVisibleServiceCache } from "../index";
+
+const mockContext = (input: unknown): Context =>
+  (({
+    bindings: {
+      visibleServicesBlob: input
+    },
+    log: console
+  } as unknown) as Context);
+
+describe("UpdateVisibleServiceCache", () => {
+  it.each`
+    scenario            | input | visibleServicesCacheBlob | visibleServicesByScopeCacheBlob
+    ${"on empty input"} | ${{}} | ${{ items: [] }}         | ${{ LOCAL: [], NATIONAL: [] }}
+  `(
+    "should succeed $title",
+    async ({
+      input,
+      visibleServicesCacheBlob,
+      visibleServicesByScopeCacheBlob
+    }) => {
+      const context = mockContext(input);
+
+      await updateVisibleServiceCache(context);
+
+      expect(context.bindings.visibleServicesCacheBlob).toEqual(
+        visibleServicesCacheBlob
+      );
+      expect(context.bindings.visibleServicesByScopeCacheBlob).toEqual(
+        visibleServicesByScopeCacheBlob
+      );
+    }
+  );
+
+  it.each`
+    scenario              | input
+    ${"on invalid input"} | ${"not-a-valid-input"}
+  `("should fail $title", async ({ input }) => {
+    const context = mockContext(input);
+
+    await updateVisibleServiceCache(context);
+
+    // out bindings aren't used
+    expect(context.bindings.visibleServicesCacheBlob).toBe(undefined);
+    expect(context.bindings.visibleServicesByScopeCacheBlob).toBe(undefined);
+  });
+});
