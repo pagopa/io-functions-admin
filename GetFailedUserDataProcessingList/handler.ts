69ac9a06fdec1ec069de14405ad109a92d16efe7
Emanuele De Cupis <ema.decu@gmail.com>
2021-09-13 14:55:26 +0200

    [#ip-387] update GetFailedUserDataProcessingList

--- GetFailedUserDataProcessingList/handler.ts
+++ GetFailedUserDataProcessingList/handler.ts
@@ -1,5 +1,8 @@
 import * as express from "express";
 import * as t from "io-ts";
+import * as E from "fp-ts/lib/Either";
+import * as TE from "fp-ts/lib/TaskEither";
+import { pipe } from "fp-ts/lib/function";
 import { Context } from "@azure/functions";
 import { ContextMiddleware } from "@pagopa/io-functions-commons/dist/src/utils/middlewares/context_middleware";
 import { RequiredParamMiddleware } from "@pagopa/io-functions-commons/dist/src/utils/middlewares/required_param";
@@ -12,13 +15,11 @@ import {
   ResponseSuccessJson
 } from "@pagopa/ts-commons/lib/responses";
 import { ServiceResponse, TableQuery, TableService } from "azure-storage";
-import { toError } from "fp-ts/lib/Either";
 import {
   IResponseErrorInternal,
   ResponseErrorInternal
 } from "@pagopa/ts-commons/lib/responses";
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
-import { tryCatch } from "fp-ts/lib/TaskEither";
 
 type TableEntry = Readonly<{
   readonly RowKey: Readonly<{
@@ -46,31 +47,31 @@ export const GetFailedUserDataProcessingListHandler = (
     .select("RowKey")
     .where("PartitionKey == ?", choice);
 
-  return tryCatch(
-    () =>
-      new Promise<TableService.QueryEntitiesResult<TableEntry>>(
-        (resolve, reject) =>
-          tableService.queryEntities(
-            failedUserDataProcessingTable,
-            tableQuery,
-            null,
-            (
-              error: Error,
-              result: TableService.QueryEntitiesResult<TableEntry>,
-              response: ServiceResponse
-            ) => (response.isSuccessful ? resolve(result) : reject(error))
-          )
-      ),
-    toError
-  )
-    .map(rs => ({
+  return pipe(
+    TE.tryCatch(
+      () =>
+        new Promise<TableService.QueryEntitiesResult<TableEntry>>(
+          (resolve, reject) =>
+            tableService.queryEntities(
+              failedUserDataProcessingTable,
+              tableQuery,
+              null,
+              (
+                error: Error,
+                result: TableService.QueryEntitiesResult<TableEntry>,
+                response: ServiceResponse
+              ) => (response.isSuccessful ? resolve(result) : reject(error))
+            )
+        ),
+      E.toError
+    ),
+    TE.map(rs => ({
       failedDataProcessingUsers: rs.entries.map(e => e.RowKey._)
-    }))
-    .fold<IResponseSuccessJson<ResultSet> | IResponseErrorInternal>(
-      er => ResponseErrorInternal(er.message),
-      ResponseSuccessJson
-    )
-    .run();
+    })),
+    TE.mapLeft(er => ResponseErrorInternal(er.message)),
+    TE.map(ResponseSuccessJson),
+    TE.toUnion
+  )();
 };
 
 export const GetFailedUserDataProcessingList = (
