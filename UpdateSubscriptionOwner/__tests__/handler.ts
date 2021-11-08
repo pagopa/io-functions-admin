1274812e05d45ea6230b3cd629a11b6bbd271c24
gquadrati <greta.quadrati@pagopa.it>
2021-11-08 17:14:04 +0100

    add new endpoint UdateSubscriptionOwner

---
+++ UpdateSubscriptionOwner/__tests__/handler.ts
@@ -0,0 +1,5 @@
+describe("UpdateSubscriptionOwner", async () => {
+  it("ok", async () => {
+    expect(1).toEqual(1);
+  });
+});
---
+++ UpdateSubscriptionOwner/index.ts
@@ -0,0 +1,38 @@
+import { Context } from "@azure/functions";
+
+import * as express from "express";
+import * as winston from "winston";
+
+import { secureExpressApp } from "@pagopa/io-functions-commons/dist/src/utils/express";
+import { AzureContextTransport } from "@pagopa/io-functions-commons/dist/src/utils/logging";
+import { setAppContext } from "@pagopa/io-functions-commons/dist/src/utils/middlewares/context_middleware";
+
+import createAzureFunctionHandler from "@pagopa/express-azure-functions/dist/src/createAzureFunctionsHandler";
+
+import { UpdateSubscriptionOwner } from "./handler";
+
+// eslint-disable-next-line functional/no-let
+let logger: Context["log"] | undefined;
+const contextTransport = new AzureContextTransport(() => logger, {
+  level: "debug"
+});
+winston.add(contextTransport);
+
+// Setup Express
+const app = express();
+secureExpressApp(app);
+
+// Add express route
+app.patch("/subscriptions/:subscriptionId/owner", UpdateSubscriptionOwner());
+
+const azureFunctionHandler = createAzureFunctionHandler(app);
+
+// Binds the express app to an Azure Function handler
+// eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+function httpStart(context: Context): void {
+  logger = context.log;
+  setAppContext(app, context);
+  azureFunctionHandler(context);
+}
+
+export default httpStart;
