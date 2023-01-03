ad4642c0b7534cca6cdb5226945d925a0363f81c
Alessio Dore <57567806+AleDore@users.noreply.github.com>
2023-01-03 15:38:22 +0100

    Fix agent for both HTTP/HTTPS support (#208)

--- utils/fetch.ts
+++ utils/fetch.ts
@@ -1,4 +1,4 @@
-import { agent } from "@pagopa/ts-commons";
+import { getFetch } from "@pagopa/ts-commons/lib/agent";
 import {
   AbortableFetch,
   setFetchTimeout,
@@ -9,12 +9,12 @@ import { Millisecond } from "@pagopa/ts-commons/lib/units";
 // HTTP external requests timeout in milliseconds
 const DEFAULT_REQUEST_TIMEOUT_MS = 10000;
 
-// HTTP-only fetch with optional keepalive agent
+// Generic HTTP/HTTPS fetch with optional keepalive agent
 // @see https://github.com/pagopa/io-ts-commons/blob/master/src/agent.ts#L10
-const httpApiFetch = agent.getHttpFetch(process.env);
+const httpOrHttpsApiFetch = getFetch(process.env);
 
 // a fetch that can be aborted and that gets cancelled after fetchTimeoutMs
-const abortableFetch = AbortableFetch(httpApiFetch);
+const abortableFetch = AbortableFetch(httpOrHttpsApiFetch);
 
 export const timeoutFetch = toFetch(
   setFetchTimeout(DEFAULT_REQUEST_TIMEOUT_MS as Millisecond, abortableFetch)
