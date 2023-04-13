e496fbcef1c680ebd5d09c78fe26161d5bb62cd8
Andrea Cuiuli <andrea.cuiuli@pagopa.it>
2023-04-13 16:50:45 +0200

    added comment to catch in test

--- utils/__tests__/retry.test.ts
+++ utils/__tests__/retry.test.ts
@@ -115,9 +115,10 @@ describe("withRetry", () => {
     const operation = anAlwaysBadOperation;
     const waitFor = 1000;
     const retriable = withRetry({ delayMS: waitFor })(operation);
-    const _ = retriable();
-    _.catch(e => e);
-    
+    const result = retriable();
+    // this catch is only to manage the unhandled promise errore
+    result.catch(e => e);
+
     expect(operation).toBeCalledTimes(1);
     await sleep(waitFor * 1.01);
 
