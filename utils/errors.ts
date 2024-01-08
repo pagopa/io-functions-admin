50b35bd3c5975b6ba359a4e3ffd8f525f9ef3ebe
Luca Cavallaro <4357400+lucacavallaro@users.noreply.github.com>
2024-01-08 15:27:44 +0100

    fix errors

--- utils/errors.ts
+++ utils/errors.ts
@@ -1,14 +1,14 @@
 import { pipe } from "fp-ts/lib/function";
 import { CosmosErrors } from "@pagopa/io-functions-commons/dist/src/utils/cosmosdb_model";
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
-import { errorsToReadableMessages } from "@pagopa/ts-commons/lib/reporters";
+import { readableReportSimplified } from "@pagopa/ts-commons/lib/reporters";
 
 export const cosmosErrorsToString = (errs: CosmosErrors): NonEmptyString =>
   pipe(
     errs.kind === "COSMOS_EMPTY_RESPONSE"
       ? "Empty response"
       : errs.kind === "COSMOS_DECODING_ERROR"
-      ? "Decoding error: " + errorsToReadableMessages(errs.error).join("/")
+      ? "Decoding error: " + readableReportSimplified(errs.error)
       : errs.kind === "COSMOS_CONFLICT_RESPONSE"
       ? "Conflict error"
       : "Generic error: " + JSON.stringify(errs.error),
