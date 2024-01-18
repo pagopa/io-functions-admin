96ad818e7c7181a102c80ecaa9c63de4e26f8681
Luca Cavallaro <luca.cavallaro@posteo.net>
2024-01-18 15:15:32 +0100

    [IOPID-1032] Add SanitizeProfileEmail to make profiles compliant with the unique email enforcement

---
+++ SanitizeProfileEmail/handler.ts
@@ -0,0 +1,92 @@
+import * as t from "io-ts";
+
+import { EmailString, FiscalCode } from "@pagopa/ts-commons/lib/strings";
+
+import * as RTE from "fp-ts/lib/ReaderTaskEither";
+import * as TE from "fp-ts/lib/TaskEither";
+import * as O from "fp-ts/lib/Option";
+
+import { pipe, flow } from "fp-ts/lib/function";
+
+import {
+  ProfileModel,
+  RetrievedProfile
+} from "@pagopa/io-functions-commons/dist/src/models/profile";
+
+import * as L from "@pagopa/logger";
+
+import { cosmosErrorsToString } from "../utils/errors";
+
+export const ProfileToSanitize = t.type({
+  email: EmailString,
+  fiscalCode: FiscalCode
+});
+
+export type ProfileToSanitize = t.TypeOf<typeof ProfileToSanitize>;
+
+interface IProfileModel {
+  readonly profileModel: ProfileModel;
+}
+
+const getProfile = (fiscalCode: ProfileToSanitize["fiscalCode"]) => (
+  r: IProfileModel
+): TE.TaskEither<Error, O.Option<RetrievedProfile>> =>
+  pipe(
+    r.profileModel.findLastVersionByModelId([fiscalCode]),
+    TE.mapLeft(flow(cosmosErrorsToString, Error))
+  );
+
+const isProfileEligibleForUpdate = (duplicatedEmail: EmailString) => (
+  profile: RetrievedProfile
+): boolean =>
+  profile.isEmailValidated === true && profile.email === duplicatedEmail;
+
+const getProfileForUpdate = (
+  profile: ProfileToSanitize
+): RTE.ReaderTaskEither<IProfileModel, Error, O.Option<RetrievedProfile>> =>
+  pipe(
+    getProfile(profile.fiscalCode),
+    RTE.map(O.filter(isProfileEligibleForUpdate(profile.email)))
+  );
+
+// after this date, e-mail notification of IO messages becomes an opt-in feature
+// so we should set "isEmailEnabled: false" to profiles that haven't updated before
+// this date.
+const OPT_OUT_EMAIL_SWITCH_DATE = 1625781600;
+
+const updateProfile = (profile: RetrievedProfile) => (
+  r: IProfileModel
+): TE.TaskEither<Error, void> =>
+  pipe(
+    r.profileModel.update({
+      ...profile,
+      isEmailEnabled:
+        // eslint-disable-next-line no-underscore-dangle
+        profile._ts < OPT_OUT_EMAIL_SWITCH_DATE
+          ? false
+          : profile.isEmailEnabled,
+      isEmailValidated: false
+    }),
+    TE.mapLeft(flow(cosmosErrorsToString, Error)),
+    TE.map(() => void 0)
+  );
+
+export const sanitizeProfileEmail = flow(
+  getProfileForUpdate,
+  RTE.chainFirstW(maybe =>
+    L.debugRTE("profile retrieved", {
+      isProfileEligibleForUpdate: O.isSome(maybe)
+    })
+  ),
+  RTE.chain(
+    flow(
+      O.map(
+        flow(
+          updateProfile,
+          RTE.chainFirstW(() => L.debugRTE("profile updated"))
+        )
+      ),
+      O.getOrElse(() => RTE.right(void 0))
+    )
+  )
+);
---
+++ SanitizeProfileEmail/index.ts
@@ -0,0 +1,24 @@
+import * as H from "@pagopa/handler-kit";
+import { azureFunction } from "@pagopa/handler-kit-azure-func";
+
+import {
+  PROFILE_COLLECTION_NAME,
+  ProfileModel
+} from "@pagopa/io-functions-commons/dist/src/models/profile";
+
+import { cosmosdbInstance } from "../utils/cosmosdb";
+
+import { ProfileToSanitize, sanitizeProfileEmail } from "./handler";
+
+const profilesContainer = cosmosdbInstance.container(PROFILE_COLLECTION_NAME);
+
+const profileModel = new ProfileModel(profilesContainer);
+
+const createSanitizeProfileEmailsFunction = azureFunction(
+  H.of(sanitizeProfileEmail)
+);
+
+export default createSanitizeProfileEmailsFunction({
+  inputDecoder: ProfileToSanitize,
+  profileModel
+});
