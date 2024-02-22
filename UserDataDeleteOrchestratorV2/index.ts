d34faea45706b6fbf4f3a9fcf295f42d32b402d5
gquadrati <greta.quadrati@pagopa.it>
2024-02-22 15:48:51 +0100

    [#IOPID-1471] add new condition for reducing delete grace period

--- UserDataDeleteOrchestratorV2/index.ts
+++ UserDataDeleteOrchestratorV2/index.ts
@@ -1,6 +1,9 @@
 import * as df from "durable-functions";
 import { Day } from "@pagopa/ts-commons/lib/units";
-import { getConfigOrThrow } from "../utils/config";
+import {
+  getConfigOrThrow,
+  isUserEligibleForInstantDelete
+} from "../utils/config";
 import { createUserDataDeleteOrchestratorHandler } from "./handler";
 
 const config = getConfigOrThrow();
@@ -8,7 +11,10 @@ const config = getConfigOrThrow();
 const waitInterval = (config.USER_DATA_DELETE_DELAY_DAYS as unknown) as Day;
 
 const orchestrator = df.orchestrator(
-  createUserDataDeleteOrchestratorHandler(waitInterval)
+  createUserDataDeleteOrchestratorHandler(
+    waitInterval,
+    isUserEligibleForInstantDelete(config)
+  )
 );
 
 export default orchestrator;
