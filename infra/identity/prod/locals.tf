d2cc75607ca6508763cbac5632b26386f6eaf7cc
Mario Mupo <mario.mupo@pagopa.it>
2024-09-03 17:43:28 +0200

    fix: edit identity name

--- infra/identity/prod/locals.tf
+++ infra/identity/prod/locals.tf
@@ -2,7 +2,6 @@ locals {
   prefix    = "io"
   env_short = "p"
   env       = "prod"
-  location  = "westeurope"
   project   = "${local.prefix}-${local.env_short}"
   domain    = "functions-admin"
 
