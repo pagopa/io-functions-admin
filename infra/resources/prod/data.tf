9558674e09dc5a27441e989c0353d778116954eb
Mario Mupo <43968294+mamu0@users.noreply.github.com>
2024-11-18 16:57:41 +0100

    [CES-68] Added configuration for APIM migration to itn (#246)

--- infra/resources/prod/data.tf
+++ infra/resources/prod/data.tf
@@ -3,6 +3,11 @@ data "azurerm_api_management" "apim" {
   resource_group_name = "${local.project_legacy}-rg-internal"
 }
 
+data "azurerm_api_management" "apim_itn" {
+  name                = local.apim_itn.name
+  resource_group_name = local.apim_itn.resource_group_name
+}
+
 data "azurerm_key_vault" "common" {
   name                = "${local.project_legacy}-kv-common"
   resource_group_name = "${local.project_legacy}-rg-common"
--- infra/resources/prod/locals.tf
+++ infra/resources/prod/locals.tf
@@ -18,4 +18,14 @@ locals {
     ManagementTeam = "IO Platform"
     Source         = "https://github.com/pagopa/io-functions-admin/blob/main/infra/resources/prod"
   }
+
+  # ITN
+  location_itn       = "italynorth"
+  location_short_itn = "itn"
+  project_itn        = "${local.prefix}-${local.env_short}-${local.location_short_itn}"
+
+  apim_itn = {
+    name                = "${local.project_itn}-apim-01"
+    resource_group_name = "${local.project_itn}-common-rg-01"
+  }
 }
