67a9339f299a2624d98779287871c6e4f7873ad3
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-08-04 08:50:31 +0200

    upgrade providers

--- infra/resources/prod/main.tf
+++ infra/resources/prod/main.tf
@@ -10,7 +10,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0.0"
+      version = "< 5.0.0"
     }
     dx = {
       source  = "pagopa-dx/azure"
